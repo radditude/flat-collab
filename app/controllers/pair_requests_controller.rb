a56c6f164fb9fcd40152e817e10bc53db1b1d548
CJ <thecjhorton@gmail.com>
2016-12-03 00:38:04 -0600

    add new pair request form

--- app/controllers/pair_requests_controller.rb
+++ app/controllers/pair_requests_controller.rb
@@ -1,2 +1,25 @@
 class PairRequestsController < ApplicationController
+  def create
+    current_user.pair_requests.create(pair_request_params)
+    redirect_to root_path
+  end
+
+  def update
+    @request = PairRequest.find(params[:id])
+    @request.mark_inactive
+    @request.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @request = PairRequest.find(params[:id])
+    @request.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def pair_request_params
+    params.require(:pair_request).permit(:project, :info)
+  end
 end
