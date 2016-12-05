52c0b6c417b4a68ca27a033512259b44ab1f035b
Clare Horton <clare.e.horton@gmail.com>
2016-12-04 22:37:26 -0600

    added join team form

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  resources :tasks
-  resources :teams
-  resources :pair_requests
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
+
   root 'welcome#home'
+
+  resources :teams do
+    resources :tasks
+  end
+
+  resources :pair_requests, only: [:create, :update, :destroy]
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
