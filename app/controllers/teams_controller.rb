859b6ac5f142d4279d908b4b508743c9d4039c9c
CJ <thecjhorton@gmail.com>
2016-12-04 22:59:51 -0600

    stubbed out team_tasks view

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,12 @@
 class TeamsController < ApplicationController
   def create
-    
+    team = Team.create(team_params)
+    redirect_to team_tasks_path(team)
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :user_ids)
   end
 end
