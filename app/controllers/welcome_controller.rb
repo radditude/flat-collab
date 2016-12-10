75f655add2aa739a506123cd9e0ea6a48bfb115c
Clare <clare.e.horton@gmail.com>
2016-12-10 21:25:08 +0000

    fixed problem with join team button on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def home
     if current_user
       @request = PairRequest.new
-      @requests = PairRequest.all.where(status: "active").persisted
+      @requests = PairRequest.all.where(status: "active").persisted.order("created_at DESC")
       render 'pair_requests/index'
     else
       render 'home'
