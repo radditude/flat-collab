539416bf09df6ad686fc86baa2c4afe3885febdf
Clare <clare.e.horton@gmail.com>
2016-12-14 23:40:42 +0000

    configured FactoryGirl for use with auth specs

--- spec/support/pair_request_factory.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-    factory :pair_request do
-        project "Test Project!"
-        info "the best project"
-        user
-    end
-end
--- spec/support/team_factory.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-    factory :team do
-        name "Test Project!"
-    end
-end
