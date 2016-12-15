75165bc3becdb69ccbdf08c2949baa5edbb70180
CJ <thecjhorton@gmail.com>
2016-12-15 01:16:16 -0600

    refactor user model tests with factories

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "Associations" do
-    it 'has many PairRequests' do
-      user = User.create(name: "Test", email: "testy@test.com", password: "passwordy")
-      request = user.pair_requests.create(project: "A Lesson")
+    before do
+      @user = create(:user)
+    end
 
-      expect(user.pair_requests).to include(request)
+    it 'has many PairRequests' do
+      @pair_request = build(:pair_request)
+      @user.pair_requests << @pair_request
+
+      expect(@user.pair_requests).to include(@pair_request)
     end
 
     it 'has many Teams' do
-      user = User.create(name: "test", email:"testy@test.com", password: "testtest")
-      team = Team.create(name: "the best team")
-      team2 = Team.create(name: "the also best team")
-      user.teams << team
-      user.teams << team2
+      team = build(:team)
+      team2 = build(:team)
+      @user.teams << team
+      @user.teams << team2
 
-      expect(user.teams).to include(team, team2)
+      expect(@user.teams).to include(team, team2)
     end
 
     it 'has many Tasks' do
-      user = User.create(name: "test", email:"testy@test.com", password: "testtest")
-      task = Task.create(name: "the task")
-      task2 = Task.create(name: "the other task")
-      task.assign_user(user)
-      task2.assign_user(user)
+      team = build(:team)
+      task = build(:task)
+      task2 = build(:task)
+      team.tasks << task
+      team.tasks << task2
+      task.assign_user(@user)
+      task2.assign_user(@user)
 
-      expect(user.tasks).to include(task, task2)
+      expect(@user.tasks).to include(task, task2)
     end
-
-  end
 end
