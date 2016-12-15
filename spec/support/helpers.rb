356f027a33fd25aec2e05a1a95c342ec65491000
Clare <clare.e.horton@gmail.com>
2016-12-15 02:05:04 -0600

    full feature test coverage!

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -7,15 +7,21 @@ module Helpers
       fill_in "user_password", with: @user.password
       click_button "Log in"
     end
-    
+
     def add_pair_request
       fill_in "pair_request_project", with: "Another test project!"
       fill_in "pair_request_info", with: "some info"
       click_button "Add Listing"
     end
-    
+
     def go_to_team_tasks
       click_button "Join Team!"
     end
-  
+
+    def add_task
+      fill_in "task_name", with: "test the tests"
+      fill_in "task_notes", with: "notes on testing the tests"
+      click_button "Submit"
+    end
+
 end
