1c440641746af521e881dc409eae86c0e432492d
Clare <clare.e.horton@gmail.com>
2016-12-15 00:48:40 -0600

    fine-tune styling

--- spec/features/team_tasks_index_spec.rb
+++ spec/features/team_tasks_index_spec.rb
@@ -11,10 +11,11 @@ RSpec.describe "team tasks index page", type: :feature do
     @team.save
     visit team_tasks_path(@team)
   end
-  
-  it 'works' do
+
+  it 'shows the task list' do
       expect(page).to have_content("to do")
+      expect(page).to have_content("")
   end
 
-  
+
 end
