b6896712b3d76ed90e2534653526f1126e53c2d2
CJ <thecjhorton@gmail.com>
2016-12-19 14:08:38 -0600

    refactor Teams#create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,9 @@
 class TeamsController < ApplicationController
   def create
-    # team = Team.create(team_params)
-    # @request = PairRequest.find(params[:pair_request][:id])
-    # @request.mark_inactive
-
     @request = PairRequest.find(params[:id])
-    @team = Team.new(name: @request.project)
-    @team.users << @request.user
-    @team.users << current_user
-    @team.save
+    @team = Team.create_from_pair_request(@request, current_user)
     @request.mark_inactive
 
     redirect_to team_tasks_path(@team)
-  end
-
-  private
-
-  def team_params
-    params.require(:team).permit(:name, :user_ids => [])
   end
 end
