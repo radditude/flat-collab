cd339a3850e5a47299ccb5f00b68ae69cef9280f
Clare <clare.e.horton@gmail.com>
2016-12-19 14:08:38 -0600

    refactor Teams#create action

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,4 +4,12 @@ class Team < ApplicationRecord
   has_many :user_teams
   has_many :users, through: :user_teams
   has_many :tasks
+
+  def self.create_from_pair_request(pair_request, other_user)
+    team = self.new(name: pair_request.project)
+    team.users << pair_request.user
+    team.users << other_user
+    team.save
+    team
+  end
 end
