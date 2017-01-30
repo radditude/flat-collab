49745ff741f3a0d45805eb632ebfff2b28bbfbce
CJ <thecjhorton@gmail.com>
2017-01-29 23:51:03 -0600

    ajax request + basic js object for pair_request index page

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  loadPairRequests();
+})
+
+class PairRequest {
+  constructor(data) {
+    Object.assign(this, data)
+  }
+
+  postedAt() {
+    var date = new Date(this.updated_at);
+    var day = date.getDate();
+    var month = date.getMonth();
+    return `<p class="small italic">Posted ${month + 1}/${day}</p>`;
+  }
+
+  formatTitle() {
+    return `<div class="header">${this.user.name} is looking for a parter for ${this.project}</div><br>`;
+  }
+
+  formatInfo() {
+    return `${this.info}`;
+  }
+
+  formatHTML() {
+    return `<div class="item"><br><div class="content">${this.formatTitle()} ${this.formatInfo()} ${this.postedAt()}</div><br></div>`;
+  }
+
+  joinButton() {
+    
+  }
+}
+
+let loadPairRequests = function() {
+  $.get("/pair_requests", function(data){
+    var posts = data.pair_requests;
+    $(posts).each(function(index, post) {
+      var thisPost = new PairRequest(post);
+      $("#pairRequests").prepend(thisPost.formatHTML());
+    });
+  })
+}
--- app/assets/javascripts/menu.js
+++
@@ -1,9 +0,0 @@
-$('.ui.menu a.item')
-  .on('click', function() {
-    $(this)
-      .addClass('active')
-      .siblings()
-      .removeClass('active')
-    ;
-  })
-;
